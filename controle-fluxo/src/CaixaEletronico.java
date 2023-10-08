0765c0c8e06f8cd28c70babbcabac8f392df4f9e
Bruno Parga <brunoparga@hotmail.com>
2023-10-08 10:51:51 -0300

    CaixaEletronico

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 25.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado <= saldo){
             saldo = saldo - valorSolicitado;
-
-        System.out.println("Saldo atual: " + saldo);
+            System.out.println("Saldo atual: R$" + saldo);
+        }else{
+            System.out.println("Saldo insuficiente para saldo: R$" + saldo);
+        }
     }
 }
