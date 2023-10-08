b595c6ba8273ca80e5c962633b3fcb1401e9c7ce
Bruno Parga <brunoparga@hotmail.com>
2023-10-08 17:45:35 -0300

    ExemploFor

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -2,12 +2,18 @@ public class ExemploForArray {
     public static void main(String[] args) {
 
         //controle de fluxo for , para interagir sobre arrays e coleções:
+        //Em arrays o indice incia sempre em ZERO
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
         //Aqui ele vai percorrer todo o vetor alunos[]
 
         for (int x=0; x<alunos.length; x++) {
             System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+
+        //Forma abreviada do for each
+        for(String aluno : alunos) {
+            System.out.println("Forma abreviada: " + aluno);
         }
 
     }
